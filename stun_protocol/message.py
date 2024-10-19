a4646f2b335e2ae12d5d169b616ef6203fe68213
Nathan Buckles <nbuckles@gmail.com>
2024-10-19 14:24:00 -0500

    When creating or unpacking a Message object, give option for whether to unpack undefined attributes or raise exception (default).  Bump version to 0.0.8

--- stun_protocol/message.py
+++ stun_protocol/message.py
@@ -83,7 +83,8 @@ class Message():
 
     def _unpack_message_type(self, message_type) -> None:
         # message class is encoded into the 11th and 7th bits
-        self.message_class = ((message_type & 0x0100) >> 7) | ((message_type & 0x0010) >> 4)
+        self.message_class = ((message_type & 0x0100) >> 7) | (
+            (message_type & 0x0010) >> 4)
 
         # message method is encoded into the 2nd-6th, 8th-10th, and 12th-15th bits
         self.message_method = ((message_type & 0x3E00) >> 2) | (
@@ -97,8 +98,9 @@ class Message():
         return struct.pack(self.STRUCT_HEADER_FORMAT, self._pack_message_type(), self.message_length,
                            self.MAGIC_COOKIE, self.transaction_id) + packed_attributes
 
-    def unpack(self, buffer: bytes):
-        (message_type, message_length, _, self.transaction_id) = struct.unpack(self.STRUCT_HEADER_FORMAT, buffer[:20])
+    def unpack(self, buffer: bytes, unpack_undefined_attributes: bool = False):
+        (message_type, message_length, _, self.transaction_id) = struct.unpack(
+            self.STRUCT_HEADER_FORMAT, buffer[:20])
         self._unpack_message_type(message_type)
 
         remaining_buffer = buffer[20:]
@@ -109,15 +111,16 @@ class Message():
 
         offset = 0
         while offset < remaining_length:
-            a = attribute.create(remaining_buffer[offset:])
+            a = attribute.create(
+                remaining_buffer[offset:], unpack_undefined_attributes)
 
             self.attributes.append(a)
             offset += a.packed_length()
 
     @classmethod
-    def create(cls: 'Message', buffer: bytes) -> 'Message':
+    def create(cls: 'Message', buffer: bytes, unpack_undefined_attributes: bool = False) -> 'Message':
         m = Message(MessageClass.REQUEST, MessageMethod.BINDING)
-        m.unpack(buffer)
+        m.unpack(buffer, unpack_undefined_attributes)
         return m
 
     def add_attribute(self, attribute: Type[Attribute]) -> None:
@@ -126,26 +129,30 @@ class Message():
 
     def add_message_integrity_attribute(self, key: bytes) -> None:
         # 1. add a message integrity attribute with an arbitrary value
-        mia = MessageIntegrityAttribute(b'0' * MessageIntegrityAttribute.fixed_length())
+        mia = MessageIntegrityAttribute(
+            b'0' * MessageIntegrityAttribute.fixed_length())
         self.attributes.append(mia)
 
         # 2. pack the stun message
         packed_message = self.pack()
 
         # 3. run hmac-sha1 over the packed stun message, up to the start of the message integrity attribute itself
-        mia_value = hmac.new(key, packed_message[0:-mia.packed_length()], sha1).digest()
+        mia_value = hmac.new(
+            key, packed_message[0:-mia.packed_length()], sha1).digest()
         self.attributes[-1].hmac = mia_value
 
     def add_message_integrity_sha256_attribute(self, key: bytes) -> None:
         # 1. add a message integrity sha 256 attribute with an arbitrary value
-        mia = MessageIntegritySha256Attribute(b'0' * MessageIntegritySha256Attribute.maximum_length())
+        mia = MessageIntegritySha256Attribute(
+            b'0' * MessageIntegritySha256Attribute.maximum_length())
         self.attributes.append(mia)
 
         # 2. pack the stun message
         packed_message = self.pack()
 
         # 3. run hmac-sha256 over the packed stun message, up to the start of the message integrity attribute itself
-        mia_value = hmac.new(key, packed_message[0:-mia.packed_length()], sha256).digest()
+        mia_value = hmac.new(
+            key, packed_message[0:-mia.packed_length()], sha256).digest()
         self.attributes[-1].hmac = mia_value
 
     def add_fingerprint_attribute(self) -> None:
@@ -157,7 +164,8 @@ class Message():
         packed_message = self.pack()
 
         # 3. run crc32 over the packed stun message, up to the start of the fingerprint attribute itself
-        fpa_value = binascii.crc32(packed_message[0:-fpa.packed_length()], 0) ^ 0x5354554e
+        fpa_value = binascii.crc32(
+            packed_message[0:-fpa.packed_length()], 0) ^ 0x5354554e
         self.attributes[-1].fingerprint = fpa_value
 
     def add_xor_mapped_address_attribute_v4(self, port: int, address: int) -> None:
@@ -169,5 +177,6 @@ class Message():
         address = address.to_bytes(length=4, byteorder='big')
 
         # 3. add an XorMappedAddressAttribute with the calculated values
-        xmaa = XorMappedAddressAttribute(MappedAddressAttributeBase.family_ipv4, port, address)
+        xmaa = XorMappedAddressAttribute(
+            MappedAddressAttributeBase.family_ipv4, port, address)
         self.add_attribute(xmaa)
--- tests/test_message.py
+++ tests/test_message.py
@@ -2,81 +2,103 @@ import unittest
 
 import stun_protocol.message as message
 from stun_protocol.attribute import FingerprintAttribute, IceControlledAttribute, MappedAddressAttribute, MappedAddressAttributeBase, PriorityAttribute, \
-    SoftwareAttribute, UsernameAttribute, XorMappedAddressAttribute
+    SoftwareAttribute, UsernameAttribute, XorMappedAddressAttribute, UndefinedAttribute
 
 
 class MessageTestCase(unittest.TestCase):
     def test_pack_message_type(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING)
         self.assertEqual(m._pack_message_type(), 0b000000000001)
 
-        m = message.Message(message.MessageClass.INDICATION, message.MessageMethod.BINDING)
+        m = message.Message(message.MessageClass.INDICATION,
+                            message.MessageMethod.BINDING)
         self.assertEqual(m._pack_message_type(), 0b000000010001)
 
-        m = message.Message(message.MessageClass.SUCCESS_RESPONSE, message.MessageMethod.BINDING)
+        m = message.Message(
+            message.MessageClass.SUCCESS_RESPONSE, message.MessageMethod.BINDING)
         self.assertEqual(m._pack_message_type(), 0b000100000001)
 
-        m = message.Message(message.MessageClass.ERROR_RESPONSE, message.MessageMethod.BINDING)
+        m = message.Message(message.MessageClass.ERROR_RESPONSE,
+                            message.MessageMethod.BINDING)
         self.assertEqual(m._pack_message_type(), 0b000100010001)
 
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.CONNECTION_BIND)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.CONNECTION_BIND)
         self.assertEqual(m._pack_message_type(), 0b000000001011)
 
-        m = message.Message(message.MessageClass.INDICATION, message.MessageMethod.CONNECTION_BIND)
+        m = message.Message(message.MessageClass.INDICATION,
+                            message.MessageMethod.CONNECTION_BIND)
         self.assertEqual(m._pack_message_type(), 0b000000011011)
 
-        m = message.Message(message.MessageClass.SUCCESS_RESPONSE, message.MessageMethod.CONNECTION_BIND)
+        m = message.Message(message.MessageClass.SUCCESS_RESPONSE,
+                            message.MessageMethod.CONNECTION_BIND)
         self.assertEqual(m._pack_message_type(), 0b000100001011)
 
-        m = message.Message(message.MessageClass.ERROR_RESPONSE, message.MessageMethod.CONNECTION_BIND)
+        m = message.Message(message.MessageClass.ERROR_RESPONSE,
+                            message.MessageMethod.CONNECTION_BIND)
         self.assertEqual(m._pack_message_type(), 0b000100011011)
 
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.GOOG_PING)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.GOOG_PING)
         self.assertEqual(m._pack_message_type(), 0b001000000000)
 
-        m = message.Message(message.MessageClass.INDICATION, message.MessageMethod.GOOG_PING)
+        m = message.Message(message.MessageClass.INDICATION,
+                            message.MessageMethod.GOOG_PING)
         self.assertEqual(m._pack_message_type(), 0b001000010000)
 
-        m = message.Message(message.MessageClass.SUCCESS_RESPONSE, message.MessageMethod.GOOG_PING)
+        m = message.Message(message.MessageClass.SUCCESS_RESPONSE,
+                            message.MessageMethod.GOOG_PING)
         self.assertEqual(m._pack_message_type(), 0b001100000000)
 
-        m = message.Message(message.MessageClass.ERROR_RESPONSE, message.MessageMethod.GOOG_PING)
+        m = message.Message(message.MessageClass.ERROR_RESPONSE,
+                            message.MessageMethod.GOOG_PING)
         self.assertEqual(m._pack_message_type(), 0b001100010000)
 
     def test_packed_length_no_attributes(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING)
         self.assertEqual(m.packed_length(), 20)
 
     def test_packed_length_with_attributes(self):
         una = UsernameAttribute(b'123')
         fa = FingerprintAttribute(0xA5A5A5A5)
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, attributes=[una, fa])
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, attributes=[una, fa])
         self.assertEqual(m.packed_length(), 36)
 
     def test_message_length_no_attributes(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING)
         self.assertEqual(m.message_length, 0)
 
     def test_message_length_with_attributes(self):
         una = UsernameAttribute(b'123')
         fa = FingerprintAttribute(0xA5A5A5A5)
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, attributes=[una, fa])
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, attributes=[una, fa])
         self.assertEqual(m.message_length, 16)
 
     def test_pack_no_attributes(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, b'\x01' * 12)
-        self.assertEqual(m.pack(), b'\x00\x01\x00\x00\x21\x12\xA4\x42' + b'\x01' * 12)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, b'\x01' * 12)
+        self.assertEqual(
+            m.pack(), b'\x00\x01\x00\x00\x21\x12\xA4\x42' + b'\x01' * 12)
 
     def test_pack_with_attributes(self):
         una = UsernameAttribute(b'123')
         fa = FingerprintAttribute(0xA5A5A5A5)
 
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, b'\x01' * 12, [una, fa])
-        self.assertEqual(m.pack(), b'\x00\x01\x00\x10\x21\x12\xA4\x42' + b'\x01' * 12 + una.pack() + fa.pack())
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, b'\x01' * 12, [una, fa])
+        self.assertEqual(m.pack(), b'\x00\x01\x00\x10\x21\x12\xA4\x42' +
+                         b'\x01' * 12 + una.pack() + fa.pack())
 
     def test_unpack_no_attributes(self):
-        m1 = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING)
-        m2 = message.Message(message.MessageClass.INDICATION, message.MessageMethod.CHANNEL_BIND)
+        m1 = message.Message(message.MessageClass.REQUEST,
+                             message.MessageMethod.BINDING)
+        m2 = message.Message(message.MessageClass.INDICATION,
+                             message.MessageMethod.CHANNEL_BIND)
 
         m2.unpack(m1.pack())
         self.assertEqual(m1, m2)
@@ -85,33 +107,51 @@ class MessageTestCase(unittest.TestCase):
         una = UsernameAttribute(b'123')
         fa = FingerprintAttribute(0xA5A5A5A5)
 
-        m1 = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, b'\x01' * 12, [una, fa])
-        m2 = message.Message(message.MessageClass.INDICATION, message.MessageMethod.CHANNEL_BIND)
+        m1 = message.Message(message.MessageClass.REQUEST,
+                             message.MessageMethod.BINDING, b'\x01' * 12, [una, fa])
+        m2 = message.Message(message.MessageClass.INDICATION,
+                             message.MessageMethod.CHANNEL_BIND)
 
         m2.unpack(m1.pack())
         self.assertEqual(m1, m2)
 
+    def test_unpack_with_undefined_attributes(self):
+        ua = UndefinedAttribute(0, b'\x01' * 8)
+        fa = FingerprintAttribute(0xA5A5A5A5)
+
+        m1 = message.Message(message.MessageClass.REQUEST,
+                             message.MessageMethod.BINDING, b'\x01' * 12, [ua, fa])
+        m2 = message.Message(message.MessageClass.INDICATION,
+                             message.MessageMethod.CHANNEL_BIND)
+
+        m2.unpack(m1.pack(), unpack_undefined_attributes=True)
+        self.assertEqual(m1, m2)
+
     def test_create(self):
-        m1 = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING)
+        m1 = message.Message(message.MessageClass.REQUEST,
+                             message.MessageMethod.BINDING)
         m2 = message.Message.create(m1.pack())
         self.assertEqual(m1, m2)
 
     def test_add_attribute(self):
         una = UsernameAttribute(b'123')
 
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING)
         m.add_attribute(una)
         self.assertEqual(len(m.attributes), 1)
 
     def test_add_message_integrity_attribute(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
         m.add_message_integrity_attribute(b'0' * 20)
         self.assertEqual(len(m.attributes), 1)
         self.assertEqual(
             m.attributes[0].hmac, b'\xc5\x1f\xc5\xe7\x5a\xf0\xe7\x94\x2d\xc1\xdf\x5e\xf6\x15\xcb\xbc\x39\x46\x44\x85')
 
     def test_add_message_integrity_sha256_attribute(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
         m.add_message_integrity_sha256_attribute(b'0' * 64)
         self.assertEqual(len(m.attributes), 1)
         self.assertEqual(
@@ -120,16 +160,19 @@ class MessageTestCase(unittest.TestCase):
             b'\x29\x61\x39\x42\xfb\x56')
 
     def test_add_fingerprint_attribute(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
         m.add_fingerprint_attribute()
         self.assertEqual(len(m.attributes), 1)
         self.assertEqual(m.attributes[0].fingerprint, 0xb2aaf9f6)
 
     def test_add_xor_mapped_address_attribute_v4(self):
-        m = message.Message(message.MessageClass.REQUEST, message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
+        m = message.Message(message.MessageClass.REQUEST,
+                            message.MessageMethod.BINDING, transaction_id=b'\x00' * 12)
         m.add_xor_mapped_address_attribute_v4(0x1234, 0x567890AB)
         self.assertEqual(len(m.attributes), 1)
-        self.assertEqual(m.attributes[0].family, MappedAddressAttributeBase.family_ipv4)
+        self.assertEqual(m.attributes[0].family,
+                         MappedAddressAttributeBase.family_ipv4)
         self.assertEqual(m.attributes[0].port, 0x3326)
         self.assertEqual(m.attributes[0].address, b'\x77\x6A\x34\xE9')
 
